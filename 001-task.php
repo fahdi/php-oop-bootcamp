ed756a4407e3469f319c8518ff50eb6105b316b5
Fahad Murtaza <fahdim@gmail.com>
2016-06-08 00:52:02 +0400

    Adding other properties, modifying via methods and then dumping them for preview.

--- 001-task.php
+++ 001-task.php
@@ -1,14 +1,32 @@
 <?php
 
-class Task {
-	public $description;
-    public function __construct($description)
+class Task
+{
+    public $title;
+    public $description;
+    public $completed;
+
+    public function __construct($title, $description)
     {
+        $this->title = $title;
         $this->description = $description;
+        $this->completed = false;
+    }
+
+    public function completed()
+    {
+        $this->completed = true;
     }
 }
 
-$task = new Task("This is my first task");
-$task2 = new Task("This is my second task");
+$task = new Task("This is my first task", "In hwre I'd be refreshing my OOP knowledge and finish this example. ");
+$task2 = new Task("This is my second task", "I'd work on more OOP examples and understand encapsultation better");
 
 var_dump($task2->description);
+
+$task2->completed();
+
+var_dump($task);
+
+var_dump($task2);
+
