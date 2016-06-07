acef5411f35f5bd7dde966454569d3909684514a
Fahad Murtaza <fahdim@gmail.com>
2016-06-08 01:30:25 +0400

    Inheritane 101

---
+++ 003-inheritance.php
@@ -0,0 +1,19 @@
+<?php
+
+class Mother
+{
+
+    public function getEyeCount()
+    {
+        return 2;
+    }
+
+}
+
+class Child extends Mother{
+
+
+
+}
+
+var_dump ((new Child())->getEyeCount());
