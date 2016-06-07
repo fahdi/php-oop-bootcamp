19141379f81fc70f6dd9082c31c493c948124dc2
Fahad Murtaza <fahdim@gmail.com>
2016-06-08 01:09:08 +0400

    Getters and setters

--- 002-name.php
+++ 002-name.php
@@ -10,9 +10,26 @@ class Person
         $this->name = $name;
 
     }
+
+    public function setAge($age)
+    {
+        if ($age < 18) {
+            throw new Exception("Person is not old enough");
+        }
+        $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
 }
 
 $john = new Person("John Doe");
-$john->age = 30;
+$john->setAge(19);
+
+$john->age = 15; #should not happen
 
 var_dump($john);
+
+var_dump( $john->getAge());
