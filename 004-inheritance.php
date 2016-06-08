96b50a88081ad243fb6715899331009545a18ddb
Fahad Murtaza <fahdim@gmail.com>
2016-06-09 00:37:23 +0400

    Adding a base and sub class and then overriding a method

---
+++ 004-inheritance.php
@@ -0,0 +1,29 @@
+<?php
+
+class Shape
+{
+    protected $length;
+    public function getArea()
+    {
+                return pow($this->length,2);
+    }
+}
+
+class Square extends Shape
+{
+
+}
+
+class Triangle extends Shape
+{
+    protected $base = 4;
+    protected $height = 10;
+
+    public function getArea()
+    {
+        return .5 * $this->base * $this->height;
+    }
+
+}
+
+echo (new Triangle)->getArea();
