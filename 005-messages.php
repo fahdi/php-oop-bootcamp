988a15ba89883198895c2daa9aae77c11a79c767
Fahad Murtaza <fahdim@gmail.com>
2016-06-09 18:27:05 +0400

    More in typehinting and messages

--- 005-messages.php
+++ 005-messages.php
@@ -35,6 +35,11 @@ class Business
         $this->staff->add($person);
 
     }
+
+    public function getStaffMembers()
+    {
+        return $this->staff->getMembers();
+    }
 }
 
 class Staff
@@ -53,15 +58,21 @@ class Staff
 
     public function add(Person $person)
     {
-        $this->members = $person;
+        $this->members[] = $person;
     }
 }
 
 $fahad = new Person("Fahad Murtaza");
-$staff = new Staff($fahad);
+$staff = new Staff([$fahad]);
 $universal_coders = new Business($staff);
 
-$universal_coders->hire($fahad);
 
-var_dump($staff);
+//$universal_coders->hire($fahad);
+$universal_coders->hire(new Person("Rizwan Abbasi"));
+
+//var_dump($staff);
+
+var_dump($universal_coders->getStaffMembers());
+
+
 
